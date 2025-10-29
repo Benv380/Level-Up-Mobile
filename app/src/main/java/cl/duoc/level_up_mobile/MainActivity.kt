5a26be9dd1afd1db1b077682e7e1f76f9dfd3db6
BastianAhumadaAguirre <b.ahumadaa@duocuc.cl>
2025-10-29 12:51:31 -0300

    Se añade animación al iniciar la aplicacion (LogoAnimated.kt) y se modifica setConent (MainActivity.kt)

--- app/src/main/java/cl/duoc/level_up_mobile/MainActivity.kt
+++ app/src/main/java/cl/duoc/level_up_mobile/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.Crossfade
+import androidx.compose.runtime.*
+import kotlinx.coroutines.delay
+import cl.duoc.level_up_mobile.ui.AnimatedLogo
 import cl.duoc.level_up_mobile.ui.theme.AppDuoc_Level_up_Theme
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +17,25 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContent {
             AppDuoc_Level_up_Theme {
-                AppNavHost()
+                // Estado que controla si aún mostramos el splash
+                var showSplash by remember { mutableStateOf(true) }
+
+                // Espera y cambia a la Home (NavHost)
+                LaunchedEffect(Unit) {
+                    // Ajusta el tiempo si quieres (ms)
+                    delay(1500)
+                    showSplash = false
+                }
+
+                Crossfade(targetState = showSplash, label = "splash-crossfade") { stillSplash ->
+                    if (stillSplash) {
+                        // Pantalla SOLO de logo
+                        AnimatedLogo()
+                    } else {
+                        // Tu navegación/composición normal
+                        AppNavHost()
+                    }
+                }
             }
         }
     }
